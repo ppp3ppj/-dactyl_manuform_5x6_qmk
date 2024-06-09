9686baf608680094e25b0b3bc0afe142a10b0307
ppp3ppj <ppp.3ppj@gmail.com>
2024-06-09 23:33:09 +0700

    update keymap

--- dactyl_manuform_5x6_qmk/keymap.c
+++ dactyl_manuform_5x6_qmk/keymap.c
@@ -14,8 +14,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
         KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,
                          KC_BSLS,KC_GRV,                                                       KC_LBRC, KC_RBRC,
-                                        KC_ESC,KC_SPC,                        KC_ENT, KC_BSPC,
-                                         KC_LCTL,KC_DEL,                         _______,  KC_LGUI,
+                                        KC_LCTL,KC_SPC,                        KC_ENT, KC_BSPC,
+                                         KC_LGUI,KC_DEL,                         _______,  KC_LGUI,
                                          RAISE, KC_LALT,                        KC_RALT, RAISE
     ),
 
