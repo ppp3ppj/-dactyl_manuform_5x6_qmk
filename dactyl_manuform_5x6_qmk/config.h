56558c79fca4d327ffef2cee3da090590cb4dd46
ppp3ppj <ppp.3ppj@gmail.com>
2024-07-22 22:57:50 +0700

    fix cold boot kb not work

--- dactyl_manuform_5x6_qmk/config.h
+++ dactyl_manuform_5x6_qmk/config.h
@@ -28,4 +28,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // https://github.com/qmk/qmk_firmware/issues/9697
 #define USE_SERIAL
 #define SPLIT_USB_DETECT
-#define SPLIT_USB_TIMEOUT 2000
+//#define SPLIT_USB_TIMEOUT 2000
+
+// fix a split keyboard doest't work when cold boot
+// @niohdachi - reddit: https://www.reddit.com/r/ErgoMechKeyboards/comments/11qxmn2/comment/jc7w9li/
+/*
+    Another user had similar problem and this was part of my reply:
+
+    SPLIT_USB_DETECT relies on USB data detection. During PC cold boot, USB enumeration may take longer than the default timeout (about 2000ms) for SPLIT_USB_DETECT, causing the detect to fail and the split kb becomes unresponsive.
+
+    The simple code fix is to include the following lines in config.h and tune the custom timeout values on per case basis. Keep in mind you won't be able to enter bios with this workaround.
+#define SPLIT_USB_TIMEOUT 10000
+#define SPLIT_USB_TIMEOUT_POLL 10
+*/
+#define SPLIT_USB_TIMEOUT 10000
+#define SPLIT_USB_TIMEOUT_POLL 10
+
