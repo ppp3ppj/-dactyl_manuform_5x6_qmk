d4b6a558be8a60c753be959b91e2915a35415057
ppp3ppj <ppp.3ppj@gmail.com>
2024-07-22 22:26:59 +0700

    change \ swap with 2498

--- dactyl_manuform_5x6_qmk/keymap.c
+++ dactyl_manuform_5x6_qmk/keymap.c
@@ -13,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
         KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
         KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,
-                         KC_BSLS,KC_GRV,                                                       KC_LBRC, KC_RBRC,
+                         KC_GRV, KC_BSLS,                                                       KC_LBRC, KC_RBRC,
                                         _______,KC_SPC,                        KC_ENT, KC_BSPC,
                                          KC_LGUI,KC_LCTL,                         KC_LCTL,  KC_LGUI,
                                          RAISE, KC_LALT,                        KC_RALT, RAISE
