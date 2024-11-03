e4471b9e1008bc8fcf115cb38e6545a7f96f3e49
ppp3ppj <ppp.3ppj@gmail.com>
2024-11-03 20:44:59 +0700

    update keymap reference by info.json

--- dactyl_cc/keymaps/ppp_custom/keymap.c
+++ dactyl_cc/keymaps/ppp_custom/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,   KC_E,    KC_R,    KC_T,                                         KC_Y, KC_U,    KC_I,    KC_O,   KC_P,    KC_MINUS,
         CTL_CAP, KC_A,    KC_S,   KC_D,    KC_F,    KC_G,                                         KC_H, KC_J,    KC_K,    KC_L,   LT_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,   KC_C,    KC_V,    KC_B,                                         KC_N, KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                 KC_BSLS, KC_GRV, KC_LEFT, KC_RIGHT,                                                     KC_DOWN, KC_UP, KC_LBRC,  KC_RBRC,
+                 KC_BSLS, KC_GRV, KC_LEFT, KC_RIGHT,                                                     KC_DOWN, KC_UP, KC_RBRC,  KC_LBRC,
                                                           KC_LCTL,  KC_LALT,     KC_RALT, KC_RCTL,
                                                  KC_SPC, KC_TAB, KC_LGUI,     KC_LGUI, KC_ENT,  KC_BSPC,
                                                                    TT(NAV),     KC_ESC
