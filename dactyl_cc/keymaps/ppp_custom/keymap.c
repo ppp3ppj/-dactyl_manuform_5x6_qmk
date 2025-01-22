dedc064808fb669c71438ab054ee6ab879717995
ppp3ppj <ppp.3ppj@gmail.com>
2025-01-22 21:00:03 +0700

    add: keymap to info_kinesis

--- dactyl_cc/keymaps/ppp_custom/keymap.c
+++ dactyl_cc/keymaps/ppp_custom/keymap.c
@@ -19,10 +19,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,   KC_E,    KC_R,    KC_T,                                         KC_Y, KC_U,    KC_I,    KC_O,   KC_P,    KC_MINUS,
         CTL_CAP, KC_A,    KC_S,   KC_D,    KC_F,    KC_G,                                         KC_H, KC_J,    KC_K,    KC_L,   LT_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,   KC_C,    KC_V,    KC_B,                                         KC_N, KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                 KC_BSLS, KC_GRV, KC_LEFT, KC_RIGHT,                                                     KC_DOWN, KC_UP, KC_RBRC,  KC_LBRC,
-                                                          KC_LCTL,  KC_LALT,     KC_RALT, KC_RCTL,
-                                                 KC_SPC, KC_BSPC, KC_CAPS,     KC_LGUI, KC_ENT,  KC_TAB,
-                                                                   TT(NAV),     KC_ESC
+                 KC_GRV, KC_BSLS, KC_LEFT, KC_RIGHT,                                                     KC_UP, KC_DOWN, KC_LBRC,  KC_RBRC,
+                                                          KC_LCTL,  KC_LALT,     KC_LGUI, KC_RCTL,
+                                                 KC_BSPC, KC_DEL, KC_HOME,     KC_PGUP, KC_ENT,  KC_SPC,
+                                                                   TT(NAV),     KC_PGDN
     ),
     [NAV] = LAYOUT(
         KC_F1, KC_F2,   KC_F3, KC_F4, KC_F5, KC_F6,                                           KC_F7, KC_F8,   KC_F9,  KC_F10,   KC_F11,   KC_F12,
